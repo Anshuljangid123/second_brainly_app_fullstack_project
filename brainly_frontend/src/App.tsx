0ae88459eebf78ffec9111353a7f1602700f1bda
Anshul Jangid <anshuljan2003@gmail.com>
2025-03-26 16:30:28 +0530

    completed button component

--- brainly_frontend/src/App.tsx
+++ brainly_frontend/src/App.tsx
@@ -1,36 +1,17 @@
-import { Button } from "../components/ui/Button"
+
 import './App.css'
-import { PlusIcon } from "./icons/plus_icon";
-import { ShareIcon } from "./icons/share_icon";
+import { Button } from './components/Button';
+import { PlusIcon } from './icons/plus_icon';
+import { ShareIcon } from './icons/share_icon';
+
 
 function App() {
 
   return (
     <>
-      <Button 
-        startIcon={<PlusIcon size="md"/>} 
-        endIcon={<ShareIcon size="md"/>}
-        variant="primary" 
-        size="sm" 
-        text="Share" 
-        onClick={() => {}} 
-      />
-      <Button 
-        startIcon={<PlusIcon size="md"/>} 
-        endIcon={<ShareIcon size="md"/>}
-        variant="secondary" 
-        size="lg" 
-        text="add content" 
-        onClick={() => {}} 
-      />
-      <Button 
-        startIcon={<PlusIcon size="md"/>} 
-        endIcon={<ShareIcon size="md"/>}
-        variant="primary" 
-        size="md" 
-        text="add content 2" 
-        onClick={() => {}} 
-      />
+
+      <Button variant="primary" text="text content" startIcon={<PlusIcon/>}/> 
+      <Button variant="secondary" text="share brain" startIcon={<ShareIcon/>}/> 
 
     </>
   )
